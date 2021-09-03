3f7b8a34a73e40139e96ac0d8ab5a0586c073f19
Maksim <max.alekseev04@gmail.com>
2021-09-03 17:26:39 +0300

    Add NotFound

--- src/components/NotFound/index.jsx
+++ src/components/NotFound/index.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
+import styles from './NotFound.module.sass';
+import ErrorIcon from '@material-ui/icons/Error';
 
 function NotFound (props) {
   const { history } = props;
 
   setTimeout(() => {
     history.push('/');
-  }, 5000);
+  }, 10000);
 
-  return <div>404 Not Found</div>;
+  return (
+    <div className={styles.notFound}>
+      <div className={styles.logoError}>
+        <ErrorIcon style={{ color: '#FF0000', fontSize: 300 }} />
+      </div>
+      <p className={styles.notFoundheaderText}>Page Not Found :(</p>
+      <p className={styles.notFoundText}>
+        Please check if the page address is correct
+      </p>
+    </div>
+  );
 }
 
 export default NotFound;
